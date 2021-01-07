d39de6d3a8c3519e35102e688787c0d886e14f6b
Deepankar Agrawal <deepankar_agrawal@external.mckinsey.com>
2021-01-07 19:56:43 +0530

    added seller

--- backend/app/models/seller.py
+++ backend/app/models/seller.py
@@ -9,3 +9,8 @@ class Seller(Base):
     is_active = Column(Boolean, default=False)
     user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"))
     store = relationship('Store', backref="seller")
+    name_on_income_tax = Column(String, nullable=False)
+    buisness_name = Column(String, nullable=False)
+    buisness_address_id = Column(UUID(as_uuid=True), ForeignKey("address.id"))
+    buisness_address = relationship('Address')
+    tax_id = Column(String, nullable=False)
